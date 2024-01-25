107c31e0b07721bee7d71483140632de9ec0f115
Kim Kangmin <rkdals0203@gmail.com>
2024-01-25 18:58:05 +0900

    topbar UI done

--- lib/widgets/top_bar.dart
+++ lib/widgets/top_bar.dart
@@ -4,6 +4,8 @@ import 'package:flutter_svg/flutter_svg.dart';
 class TopBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Size get preferredSize => Size.fromHeight(49); // Adjust the height accordingly
+
+  @override
   Widget build(BuildContext context) {
     return Container(
       width: 1300,
@@ -20,103 +22,132 @@ class TopBar extends StatelessWidget implements PreferredSizeWidget {
         ],
       ),
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Text(
-            '환자검색',
-            style: TextStyle(
-              color: Color(0xFF404855),
-              fontSize: 16,
-              fontFamily: 'Pretendard',
-              fontWeight: FontWeight.w700,
-              height: 0.09,
-              letterSpacing: -0.18,
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Text(
+              '환자검색',
+              style: TextStyle(
+                color: Color(0xFF404855),
+                fontSize: 16,
+                fontFamily: 'Pretendard',
+                fontWeight: FontWeight.w700,
+                height: 0.09,
+                letterSpacing: -0.18,
+              ),
             ),
           ),
-          Container(
-            width: 328,
-            height: 28,
-            decoration: ShapeDecoration(
-              color: Color(0xFFF7F7F7),
-              shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  '환자이름 /차트번호/생년월일/휴대폰번호',
-                  style: TextStyle(
-                    color: Color(0xFFAFAFAF),
-                    fontSize: 11,
-                    fontFamily: 'Pretendard',
-                    fontWeight: FontWeight.w400,
-                    height: 0.18,
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+            child: Container(
+              width: 328,
+              height: 28,
+              decoration: ShapeDecoration(
+                color: Color(0xFFF7F7F7),
+                shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                    child: Text(
+                      '환자이름 /차트번호/생년월일/휴대폰번호',
+                      style: TextStyle(
+                        color: Color(0xFFAFAFAF),
+                        fontSize: 11,
+                        fontFamily: 'Pretendard',
+                        fontWeight: FontWeight.w400,
+                        height: 0.18,
+                      ),
+                    ),
                   ),
-                ),
-                SvgPicture.asset('assets/icons/icon_search.svg'),
-              ],
-            ),
-          ),
-          Text(
-            '이수민',
-            style: TextStyle(
-              color: Color(0xFF404855),
-              fontSize: 12,
-              fontFamily: 'Pretendard',
-              fontWeight: FontWeight.w400,
-              height: 0.12,
-            ),
-          ),
-          Text(
-            '2024-05-24',
-            style: TextStyle(
-              color: Color(0xFF404855),
-              fontSize: 12,
-              fontFamily: 'Pretendard',
-              fontWeight: FontWeight.w400,
-              height: 0.12,
-              letterSpacing: 0.12,
-            ),
-          ),
-          Text(
-            '20:12:44',
-            style: TextStyle(
-              color: Color(0xFF404855),
-              fontSize: 12,
-              fontFamily: 'Pretendard',
-              fontWeight: FontWeight.w400,
-              height: 0.12,
-              letterSpacing: 0.12,
-            ),
-          ),
-          Container(
-            width: 101,
-            height: 28,
-            decoration: ShapeDecoration(
-              color: Color(0xFFE2F1F6),
-              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
-            ),
-            child:
-            Row(
-              children: [
-                Text(
-                  '신규환자',
-                  style: TextStyle(
-                    color: Color(0xFF404855),
-                    fontSize: 14,
-                    fontFamily: 'Pretendard',
-                    fontWeight: FontWeight.w400,
-                    height: 0.11,
-                    letterSpacing: -0.15,
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                    child: SvgPicture.asset('assets/icons/icon_search.svg'),
                   ),
-                ),
-                SvgPicture.asset('assets/icons/icon_addPerson.svg'),
-              ],
+                ],
+              ),
             ),
           ),
-
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Container(
+              width: 101,
+              height: 28,
+              decoration: ShapeDecoration(
+                color: Color(0xFFE2F1F6),
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text(
+                    '신규환자',
+                    style: TextStyle(
+                      color: Color(0xFF404855),
+                      fontSize: 14,
+                      fontFamily: 'Pretendard',
+                      fontWeight: FontWeight.w400,
+                      height: 0.11,
+                      letterSpacing: -0.15,
+                    ),
+                  ),
+                  SvgPicture.asset('assets/icons/icon_addPerson.svg'),
+                ],
+              ),
+            ),
+          ),
+          Spacer(), // Added Spacer to push '이수민' and the following texts to the right
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10.0),
+            child: Text(
+              '이수민',
+              style: TextStyle(
+                color: Color(0xFF404855),
+                fontSize: 12,
+                fontFamily: 'Pretendard',
+                fontWeight: FontWeight.w400,
+                height: 0.12,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10.0),
+            child: Text(
+              '2024-05-24',
+              style: TextStyle(
+                color: Color(0xFF404855),
+                fontSize: 12,
+                fontFamily: 'Pretendard',
+                fontWeight: FontWeight.w400,
+                height: 0.12,
+                letterSpacing: 0.12,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10.0),
+            child: Text(
+              '20:12:44',
+              style: TextStyle(
+                color: Color(0xFF404855),
+                fontSize: 12,
+                fontFamily: 'Pretendard',
+                fontWeight: FontWeight.w400,
+                height: 0.12,
+                letterSpacing: 0.12,
+              ),
+            ),
+          ),
+          SvgPicture.asset('assets/icons/icon_lining.svg'),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10.0),
+            child: SvgPicture.asset('assets/icons/icon_message.svg'),
+          ),
         ],
       ),
     );
