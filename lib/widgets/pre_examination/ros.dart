f24fcca12724480209fdbf959b2e49be9a8c66ce
Siheon Kim <siheon0411@gmail.com>
2024-02-18 19:04:59 +0900

    ros keyword design

--- lib/widgets/pre_examination/ros.dart
+++ lib/widgets/pre_examination/ros.dart
@@ -15,50 +15,109 @@ class Ros extends StatelessWidget {
       decoration: const BoxDecoration(color: Colors.white),
       child: Padding(
         padding: const EdgeInsets.all(10.0),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start, // 요소들을 위쪽부터 정렬
+        child: Column(
           children: [
-            const Text(
-              'ROS',
-              style: TextStyle(
-                color: Color(0xFF404855),
-                fontSize: 12,
-                fontFamily: 'Pretendard',
-                fontWeight: FontWeight.w700,
-                letterSpacing: 0.12,
-                height: 1, // 높이 설정
-              ),
-            ),
-            const Spacer(),
-            GestureDetector(
-              onTap: () {
-                showDialog(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return Align(
-                      alignment: Alignment.center,
-                      child: Padding(
-                        padding: const EdgeInsets.fromLTRB(0, 50, 0, 0),
-                        child: Dialog(
-                          elevation: 0,
-                          backgroundColor: Colors.transparent,
-                          child: AddRosScreen(chartNumber: chartNumber),
-                        ),
-                      ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start, // 요소들을 위쪽부터 정렬
+              children: [
+                const Text(
+                  'ROS',
+                  style: TextStyle(
+                    color: Color(0xFF404855),
+                    fontSize: 12,
+                    fontFamily: 'Pretendard',
+                    fontWeight: FontWeight.w700,
+                    letterSpacing: 0.12,
+                    height: 1, // 높이 설정
+                  ),
+                ),
+                const Spacer(),
+                GestureDetector(
+                  onTap: () {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return Align(
+                          alignment: Alignment.center,
+                          child: Padding(
+                            padding: const EdgeInsets.fromLTRB(0, 50, 0, 0),
+                            child: Dialog(
+                              elevation: 0,
+                              backgroundColor: Colors.transparent,
+                              child: AddRosScreen(chartNumber: chartNumber),
+                            ),
+                          ),
+                        );
+                      },
                     );
                   },
-                );
-              },
+                  child: Container(
+                    child: Row(
+                      children: [
+                        const Text('ROS 추가 -> '),
+                        const SizedBox(width: 5),
+                        SvgPicture.asset('assets/icons/icon_document.svg'),
+                        const SizedBox(width: 5),
+                        SvgPicture.asset('assets/icons/icon_right_arrow.svg'),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+            Expanded(
               child: Container(
+                width: double.infinity,
+                clipBehavior: Clip.antiAlias,
+                decoration: const BoxDecoration(),
                 child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    SvgPicture.asset('assets/icons/icon_document.svg'),
+                    Container(
+                      child: const Row(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            '# 식욕부진',
+                            style: TextStyle(
+                              color: Color(0xFF3EA7C2),
+                              fontSize: 12,
+                              fontFamily: 'Pretendard',
+                              fontWeight: FontWeight.w400,
+                              height: 0.12,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                     const SizedBox(width: 5),
-                    SvgPicture.asset('assets/icons/icon_right_arrow.svg'),
+                    Container(
+                      child: const Row(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            '# 오한',
+                            style: TextStyle(
+                              color: Color(0xFF3EA7C2),
+                              fontSize: 12,
+                              fontFamily: 'Pretendard',
+                              fontWeight: FontWeight.w400,
+                              height: 0.12,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                   ],
                 ),
               ),
-            )
+            ),
           ],
         ),
       ),
