7070365dd6377920d1315c6d36eaf9e83ef2e00a
Siheon Kim <siheon0411@gmail.com>
2024-02-21 02:01:54 +0900

    nursing note main

--- lib/widgets/main_examination/nursing_note_main.dart
+++ lib/widgets/main_examination/nursing_note_main.dart
@@ -4,16 +4,16 @@ import 'package:get/get.dart';
 import 'package:my_desktop_app/controller/pre_examination_controller.dart';
 import 'package:my_desktop_app/styles/textStyles.dart';
 
-class NursingNote extends StatelessWidget {
+class NursingNoteMain extends StatelessWidget {
   final int chartNumber;
   final double height;
   final double width;
-  NursingNote(
-      {Key? key,
+
+  NursingNoteMain(
+      {super.key,
       required this.chartNumber,
       required this.height,
-      required this.width})
-      : super(key: key);
+      required this.width});
 
   final PreExaminationController preExaminationController =
       Get.put(PreExaminationController()); //컨트롤러에 데이
@@ -50,30 +50,39 @@ class NursingNote extends StatelessWidget {
               ],
             ),
           ),
-          TextField(
-            cursorHeight: 8.0,
-            controller: nursingNoteController,
-            onChanged: (value) {
-              // mainSymptom의 값을 변경합니다.
-              preExaminationController.updateNursingNote(value);
-              print('저장하는 주증상: ${preExaminationController.nursingNote.value}');
-            },
-            maxLines: 11,
-            decoration: const InputDecoration(
-                hintStyle: TextStyle(
-                  color: Color(0xFFAFAFAF),
-                  fontSize: 11,
-                  fontFamily: 'Pretendard',
-                  fontWeight: FontWeight.w400,
-                ),
-                contentPadding: EdgeInsets.symmetric(
-                  horizontal: 10,
-                  vertical: 15,
-                ),
-                border: InputBorder.none),
-            style: TextStyles.text11Style
-                .copyWith(height: 1.5), // height 값을 조절해주세요
+          Obx(
+            () => Padding(
+              padding: const EdgeInsets.fromLTRB(10, 0, 0, 10),
+              child: Text(
+                preExaminationController.nursingNote.value,
+                style: TextStyles.text11Style.copyWith(height: 1.5),
+              ),
+            ),
           ),
+          // TextField(
+          //   cursorHeight: 8.0,
+          //   controller: nursingNoteController,
+          //   onChanged: (value) {
+          //     // mainSymptom의 값을 변경합니다.
+          //     preExaminationController.updateNursingNote(value);
+          //     print('저장하는 주증상: ${preExaminationController.nursingNote.value}');
+          //   },
+          //   maxLines: 4,
+          //   decoration: const InputDecoration(
+          //       hintStyle: TextStyle(
+          //         color: Color(0xFFAFAFAF),
+          //         fontSize: 11,
+          //         fontFamily: 'Pretendard',
+          //         fontWeight: FontWeight.w400,
+          //       ),
+          //       contentPadding: EdgeInsets.symmetric(
+          //         horizontal: 10,
+          //         vertical: 15,
+          //       ),
+          //       border: InputBorder.none),
+          //   style: TextStyles.text11Style
+          //       .copyWith(height: 1.5), // height 값을 조절해주세요
+          // ),
         ],
       ),
     );
